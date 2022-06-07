7c15b7e3a377d81fa4a1457e470b0ce5c20a4b58
hvs1234 <102682161+hvs1234@users.noreply.github.com>
2022-06-07 10:59:06 +0530

    Update Know By.py

--- Know By.py
+++ Know By.py
@@ -1,8 +1,9 @@
+from curses.textpad import Textbox
 from tkinter import *
 from tkinter import messagebox
 root = Tk()
 root.title('D-A-T-A-B-A-S-E  L-O-G-I-N ...')
-root.configure(bg='cyan')
+root.configure(bg='orange')
 root.resizable(False,False)
 root.geometry('700x500+320+80')
 
@@ -22,30 +23,41 @@ def f1():
             root2.configure(bg="grey")
             root2.geometry('700x500+350+100')
             root2.title("Server 1 ...")
-            b1 = Button(root2,text='Access Portal',width=20,font="Cambria 15 bold",bd=4,
-                        bg='grey',fg='indigo'); b1.place(x=20,y=30)
-            b2 = Button(root2,text='Server Components',width=20,font="Cambria 15 bold",bd=4,
-                        bg='grey',fg='indigo'); b2.place(x=20,y=110)
-            b3 = Button(root2,text='Data Transfer',width=20,font="Cambria 15 bold",bd=4,
-                        bg='grey',fg='indigo'); b3.place(x=20,y=190)
-            b4 = Button(root2,text='Address Transfer',width=20,font="Cambria 15 bold",bd=4,
-                        bg='grey',fg='indigo'); b4.place(x=20,y=270)
-            b5 = Button(root2,text='Request Accept',width=20,font="Cambria 15 bold",bd=4,
-                        bg='grey',fg='indigo'); b5.place(x=20,y=350)
-            b6 = Button(root2,text='Task Complete',width=20,font="Cambria 15 bold",bd=4,
-                        bg='grey',fg='indigo'); b6.place(x=20,y=430)
-            b7 = Button(root2,text='Recheck Server Data',width=20,font="Cambria 15 bold",bd=4,
-                        bg='grey',fg='indigo'); b7.place(x=370,y=30)
-            b8 = Button(root2,text='Error Detection',width=20,font="Cambria 15 bold",bd=4,
-                        bg='grey',fg='indigo'); b8.place(x=370,y=110)
-            b9 = Button(root2,text='Issue',width=20,font="Cambria 15 bold",bd=4,
-                        bg='grey',fg='indigo'); b9.place(x=370,y=190)
-            b10 = Button(root2,text='Security',width=20,font="Cambria 15 bold",bd=4,
-                        bg='grey',fg='indigo'); b10.place(x=370,y=270)
-            b11 = Button(root2,text='Definition Manager',width=20,font="Cambria 15 bold",bd=4,
-                        bg='grey',fg='indigo'); b11.place(x=370,y=350)
-            b12 = Button(root2,text='Details',width=20,font="Cambria 15 bold",bd=4,
-                        bg='grey',fg='indigo'); b12.place(x=370,y=430)
+
+            def m1():
+                root3 = Tk()
+                root3.title('Editor ...')
+                root3.configure(bg='white')
+                root3.geometry('700x500+100+50')
+                t1 = Text(root3,bd=6,bg='white',fg='black',width=700,height=400,
+                        font='Fixedsys 10').place(x=0,y=40)
+                root3.mainloop()
+
+            b1 = Button(root2,text='Data Access',width=20,bd=4,bg='grey',
+                    fg='indigo',font='Cambria 15 bold'); b1.place(x=20,y=30)
+            b2 = Button(root2,text='Server Request',width=20,bd=4,bg='grey',
+                    fg='indigo',font='Cambria 15 bold'); b2.place(x=20,y=110)
+            b3 = Button(root2,text='Data Transfer',width=20,bd=4,bg='grey',
+                    fg='indigo',font='Cambria 15 bold'); b3.place(x=20,y=190)
+            b4 = Button(root2,text='Address Transfer',width=20,bd=4,bg='grey',
+                    fg='indigo',font='Cambria 15 bold'); b4.place(x=20,y=270)
+            b5 = Button(root2,text='Requirements',width=20,bd=4,bg='grey',
+                    fg='indigo',font='Cambria 15 bold'); b5.place(x=20,y=350)
+            b6 = Button(root2,text='Error Detection',width=20,bd=4,bg='grey',
+                    fg='indigo',font='Cambria 15 bold'); b6.place(x=20,y=430)
+            b7 = Button(root2,text='Issue',width=20,bd=4,bg='grey',
+                    fg='indigo',font='Cambria 15 bold'); b7.place(x=370,y=30)
+            b8 = Button(root2,text='Connection',width=20,bd=4,bg='grey',
+                    fg='indigo',font='Cambria 15 bold'); b8.place(x=370,y=110)
+            b9 = Button(root2,text='Server Manager',width=20,bd=4,bg='grey',
+                    fg='indigo',font='Cambria 15 bold'); b9.place(x=370,y=190)
+            b10 = Button(root2,text='Task Complete',width=20,bd=4,bg='grey',
+                    fg='indigo',font='Cambria 15 bold'); b10.place(x=370,y=270)
+            b11 = Button(root2,text='Editor',width=20,bd=4,bg='grey',
+                    fg='indigo',font='Cambria 15 bold',
+                    command=m1); b11.place(x=370,y=350)
+            b12 = Button(root2,text='Details',width=20,bd=4,bg='grey',
+                    fg='indigo',font='Cambria 15 bold'); b12.place(x=370,y=430)
             root2.mainloop()
         def f2():
             root3 = Tk()
@@ -88,19 +100,23 @@ def f1():
         messagebox.showerror("Invalid Login","Invalid Username and Password")
 
 l1 = Label(root,text = "Welcome To Database ... ",font='Cambria 25 bold',
-            fg='black',bg='cyan',bd=4).place(x=120,y=50)
+            fg='black',bg='orange',bd=4).place(x=120,y=50)
 l2 = Label(root,text = "Username",font="Cambria 20 bold",
-            fg='black',bg='cyan',bd=4).place(x=120,y=160)
+            fg='black',bg='orange',bd=4).place(x=120,y=160)
 l3 = Label(root,text = "Password",font="Cambria 20 bold",
-            fg='black',bg='cyan',bd=4).place(x=120,y=250)
-e1 = Entry(root,width=20,bd=4,fg='indigo',bg='light grey',font="Cambria 15 bold")
+            fg='black',bg='orange',bd=4).place(x=120,y=250)
+s1 = StringVar()
+s2 = StringVar()
+e1 = Entry(root,textvariable=s1,width=20,bd=4,fg='indigo',bg='light grey',font="Cambria 15 bold")
 e1.place(x=300,y=160)
-e2 = Entry(root,width=20,bd=4,fg='indigo',bg='lightgrey',font="Cambria 15 bold",show="*")
+s1.set('harsh')
+e2 = Entry(root,textvariable=s2,width=20,bd=4,fg='indigo',bg='lightgrey',font="Cambria 15 bold",show="*")
 e2.place(x=300,y=250)
+s2.set('h1234')
 b1 = Button(root,text="Login",width=8,
-            bd=6,height=2,font="Cambria 15 bold",fg='black',bg='cyan',command=f1)
+            bd=6,height=2,font="Cambria 15 bold",fg='black',bg='orange',command=f1)
 b1.place(x=120,y=350)
 b2 = Button(root,text="Cancel",width=8,
-            bd=6,height=2,font="Cambria 15 bold",fg='black',bg='cyan',command=root.destroy)
+            bd=6,height=2,font="Cambria 15 bold",fg='black',bg='orange',command=root.destroy)
 b2.place(x=340,y=350)
 root.mainloop()
